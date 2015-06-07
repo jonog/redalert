9d4120c6ef20807f34bfda5ec13eeae9bfc40180
Jonathan Gomez <jonathanbgomez@gmail.com>
2015-06-07 19:17:12 +0800

    Handle error when getting container name.

--- checks/remote_docker.go
+++ checks/remote_docker.go
@@ -188,7 +188,7 @@ func (r *RemoteDocker) Check() (Metrics, error) {
 	return output, nil
 }
 
-func getContainerName(names []string) string {
+func getContainerName(names []string) (string, error) {
 
 	// remove prefix '/'
 	for _, name := range names {
@@ -196,10 +196,11 @@ func getContainerName(names []string) string {
 
 		// find container without '/' within name
 		if len(strings.Split(namePrefixRemoved, "/")) == 1 {
-			return namePrefixRemoved
+			return namePrefixRemoved, nil
 		}
 	}
-	return ""
+
+	return "", errors.New("remote-docker: unable to find container name")
 }
 
 func (r *RemoteDocker) MetricInfo(metric string) MetricInfo {
