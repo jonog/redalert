28e1c3a8f3d57ec9acd27e8269ab8f1e7990c75b
Jonathan Gomez <jonathanbgomez@gmail.com>
2016-06-29 08:31:19 +1000

    Endpoint to enable/disable a check (#30)

---
+++ web/check_handler.go
@@ -0,0 +1,49 @@
+package web
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func checkDisableHandler(c *appCtx, w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id := vars["check_id"]
+
+	check, err := c.service.CheckByID(id)
+	if err != nil {
+		http.Error(w, "Not Found", http.StatusNotFound)
+		return
+	}
+
+	if !check.Enabled {
+		http.Error(w, "Check is already disabled", http.StatusPreconditionFailed)
+		return
+	}
+
+	check.Stop()
+
+	w.Write([]byte(`OK`))
+}
+
+func checkEnableHandler(c *appCtx, w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id := vars["check_id"]
+
+	check, err := c.service.CheckByID(id)
+	if err != nil {
+		http.Error(w, "Not Found", http.StatusNotFound)
+		return
+	}
+
+	if check.Enabled {
+		http.Error(w, "Check is already disabled", http.StatusPreconditionFailed)
+		return
+	}
+
+	check.Start()
+
+	w.Write([]byte(`OK`))
+}
